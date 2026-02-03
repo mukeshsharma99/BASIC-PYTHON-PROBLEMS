13730cc1f7f26cd428f5e136ba5dbab0edd76da7
mukeshsharma99 <mukesh10400@gmail.com>
2026-02-03 19:59:05 +0530

    push

---
+++ Numbers & Arithmetic/2-number is even or odd.py
@@ -0,0 +1,10 @@
+# Check if a number is even or odd.
+
+num = int(input("Enter the number:"))
+
+if num % 2 == 0:
+    print(num, "is an even numbers")
+else:
+    print(num, "is an odd numbers")
+
+    
