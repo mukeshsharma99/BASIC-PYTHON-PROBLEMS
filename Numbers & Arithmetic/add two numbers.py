b0b8549fdff43bbe026e99ec0a6c81f6a8c59331
mukeshsharma99 <mukesh10400@gmail.com>
2026-02-03 19:49:57 +0530

    push

--- Numbers & Arithmetic/add two numbers.py
+++ Numbers & Arithmetic/add two numbers.py
@@ -1,2 +1,9 @@
 # Write a program to add two numbers.
 
+num1 = int(input("Enter the first number: "))
+
+num2 = int(input("Enter the second number:"))\
+
+sum = num1 + num2
+
+print("The sum of ", num1, "And", num2, "is ", sum)
