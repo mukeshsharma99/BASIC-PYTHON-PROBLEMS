56c5ec04c683933b6e95584f12d615afcd1e3325
mukeshsharma99 <mukesh10400@gmail.com>
2026-02-05 13:03:55 +0530

    push the code

---
+++ Numbers & Arithmetic/8-Number is a palindrome.py
@@ -0,0 +1,14 @@
+# number is a palindrome
+
+num=  int(input("Enter the number: "))
+temp  = num
+reverse = 0
+
+while num > 0:
+    reverse = reverse *10 + num %10
+    num //= 10
+
+if temp == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
